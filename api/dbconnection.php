936fb0ed1f456e013f5261f182e9c9019a868c87
s-potta <s-potta@wiu.edu>
2020-11-16 15:54:18 -0500

    changed dbconnection file

--- api/dbconnection.php
+++ api/dbconnection.php
@@ -10,9 +10,9 @@ function connect()
 {
   $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
 
-  if (mysqli_connect_errno($connect)) {
-    die("Failed to connect:" . mysqli_connect_error());
-  }
+  if ($connect->connect_error) {
+    die("ERROR: Unable to connect: " . $conn->connect_error);
+  } 
 
   mysqli_set_charset($connect, "utf8");
 
