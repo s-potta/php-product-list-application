12badc420507e071f7590d3c600571f449e7cec3
s-potta <s-potta@wiu.edu>
2020-11-18 09:27:33 -0500

    Added DB Scripts

--- api/dbscripts.sql
+++ api/dbscripts.sql
@@ -1,32 +1,47 @@
-mysql -h mysql.wiu.edu -P 2521 -u root -p
-
-mysql -h mysql.wiu.edu -P 2521 -u root -p
-
-CREATE DATABASE ecommerce;
-mysql --user=sathishpotta --password=Kalikrishna$9
-CREATE USER 'ecomuser'@'10.16.240.%' IDENTIFIED BY 'ecom1@3$5';
-CREATE USER 'ecomuser'@'10.50.202.11' IDENTIFIED BY 'ecom1@3$5';
-CREATE USER 'ecomuser'@'10.50.202.15' IDENTIFIED BY 'ecom1@3$5';
-CREATE USER 'ecomuser'@'143.43.222.%' IDENTIFIED BY 'ecom1@3$5';
-GRANT ALL PRIVILEGES ON ecommerce . * TO 'ecomuser'@'10.16.240.%' ;
-GRANT ALL PRIVILEGES ON ecommerce . * TO 'ecomuser'@'10.50.202.11'
-GRANT ALL PRIVILEGES ON ecommerce . * TO 'ecomuser'@'10.50.202.15'
-GRANT ALL PRIVILEGES ON ecommerce . * TO 'ecomuser'@'143.43.222.%'
+CREATE DATABASE IF NOT EXISTS `ecommerce` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `ecommerce`;
 
 
+CREATE TABLE `comments` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `comment` varchar(255) NOT NULL DEFAULT '',
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `userid` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-USE ecommerce;
-
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `products` (
+  `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL DEFAULT '',
   `description` varchar(1000) NOT NULL DEFAULT '',
   `image` varchar(255) NOT NULL DEFAULT '',
-  `price` int (10) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `price` int(10) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO products(id, title, image, description, price) values(1,'Basic-TShirt','assets/images/Item_1.jpg','Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero''s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with',7.99);
 
-INSERT INTO products(id, title, description, image, price) values(2,'Black Glasses','assets/images/Item_1.jpg','Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero''s De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with',8.99);
+INSERT INTO `products` (`id`, `title`, `description`, `image`, `price`) VALUES
+(1, 'Basic-TShirt', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_1.jpg', 8),
+(2, 'Denim Shorts', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_2.jpg', 9),
+(3, 'Fantasy TShirt', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_3.jpg', 13),
+(4, 'Shoes', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_4.jpg', 14),
+(5, 'Fantasy Pants', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_5.jpg', 18),
+(6, 'Shoulder Bag', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_6.jpg', 15),
+(7, 'Simple Hat', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_7.jpg\r\n', 3),
+(8, 'Black Glasses', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_8.jpg', 24),
+(9, 'Havana TShirt', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_9.jpg', 19),
+(10, 'Watch', 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum e', 'assets/images/Item_10.jpg\r\n', 17);
+
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
+
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `comments` MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+ALTER TABLE `products`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
